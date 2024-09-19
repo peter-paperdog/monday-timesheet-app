edab36e900c1e43762598a3879f727c78144d592
nagybence <bence@paperdog.com>
2024-09-19 12:28:50 +0200

    Simplify login form and center elements
    Removed individual email and password fields, including their labels and error messages. Centered the login form on the screen and kept only the 'Login with Google' option. This provides a cleaner and more focused login experience.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,40 +5,10 @@
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')"/>
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
-                          autofocus autocomplete="username"/>
-            <x-input-error :messages="$errors->get('email')" class="mt-2"/>
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')"/>
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                          type="password"
-                          name="password"
-                          required autocomplete="current-password"/>
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2"/>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                   href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+        <div class="flex items-center justify-center h-screen">
             <x-primary-button class="ms-3">
-                {{ __('Log in') }}
+                <a href="/login/google" class="ms-3">Login with Google</a>
             </x-primary-button>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a href="/login/google" class="ms-3"> Login with Google </a>
         </div>
     </form>
 </x-guest-layout>
