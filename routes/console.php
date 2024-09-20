2bd6d5de43d5b78e9f032aaec268fd4ad83102be
nagybence <bence@paperdog.com>
2024-09-21 01:54:27 +0200

    Update scheduled command from 'weekly:allPdf' to 'weekly:allsummary'
    Changed the scheduled command name in the console routes file to better reflect its functionality. This ensures consistency and clarity in the naming conventions used for scheduled tasks.

--- routes/console.php
+++ routes/console.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('weekly:notification')->weeklyOn(5, '15:30')->timezone('Europe/London');
-Schedule::command('weekly:allPdf')->weeklyOn(5, '18:00')->timezone('Europe/London');
-//Schedule::command('weekly:notification')->everyMinute();
+Schedule::command('weekly:allsummary')->weeklyOn(5, '18:00')->timezone('Europe/London');
+//Schedule::command('weekly:allsummary')->everyMinute();
