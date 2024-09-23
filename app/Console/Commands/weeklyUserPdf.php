f50ece3bb71af4cbe316943480e0bacc5ac21e0e
nagybence <bence@paperdog.com>
2024-09-23 10:15:55 +0200

    Add command to generate and email weekly user PDF summary
    This commit introduces a new console command `weekly:usersummary` that generates a weekly PDF summary of user timesheets and emails it to users, excluding certain exceptions. The command gathers user data, compiles it into a PDF using `TimesheetController`, and sends the PDF via email.

---
+++ app/Console/Commands/weeklyUserPdf.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Http\Controllers\TimesheetController;
+use App\Mail\allUserSummaryEmail;
+use App\Mail\userSummaryEmail;
+use App\Services\MondayService;
+use App\Services\UserService;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class weeklyUserPdf extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'weekly:usersummary';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+
+        $mondayService = new MondayService();
+        $usersService = new UserService($mondayService);
+        $users = $usersService->getUsers();
+        $allTimeTrackingItems = $mondayService->getTimeTrackingItems();
+        $exceptions = [
+            'petra@paperdog.com', 'szonja@paperdog.com', 'oliver@paperdog.com', 'amo@paperdog.com',
+            'morwenna@paperdog.com', 'gabriella@paperdog.com'
+        ];
+
+        foreach ($users as $user) {
+            if (!in_array($user['email'], $exceptions)) {
+
+                $User = $usersService->getUserBy('email', $user['email']);
+                $TimesheetController = new TimesheetController();
+                $pdf = $TimesheetController->downloadUserTimeSheet($User, $allTimeTrackingItems);
+                info("Weekly summary sent to ".$user['email'].' '.now());
+                Mail::to($user['email'])->send(new userSummaryEmail($pdf));
+            }
+        }
+    }
+}
