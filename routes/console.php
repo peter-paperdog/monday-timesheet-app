5accaa3813b818f0bc121e94a843d16649c5895e
nagybence <bence@paperdog.com>
2024-09-30 12:44:10 +0200

    Update scheduling intervals for console commands
    Commented out old weekly schedules and replaced them with every-minute schedules for `weekly:notification`, `weekly:allsummary`, and `weekly:usersummary` commands. This change is necessary to facilitate more frequent executions for debugging or testing purposes.

--- routes/console.php
+++ routes/console.php
@@ -4,6 +4,9 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('weekly:notification')->weeklyOn(5, '15:30')->timezone('Europe/London');
+/*Schedule::command('weekly:notification')->weeklyOn(5, '15:30')->timezone('Europe/London');
 Schedule::command('weekly:allsummary')->weeklyOn(5, '18:00')->timezone('Europe/London');
-Schedule::command('weekly:usersummary')->weeklyOn(5, '18:00')->timezone('Europe/London');
+Schedule::command('weekly:usersummary')->weeklyOn(5, '18:00')->timezone('Europe/London');*/
+Schedule::command('weekly:notification')->everyMinute();
+Schedule::command('weekly:allsummary')->everyMinute();
+Schedule::command('weekly:usersummary')->everyMinute();
