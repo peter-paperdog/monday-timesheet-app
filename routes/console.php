4247274082356be0f755a07cabe29f2e3f987b62
nagybence <bence@paperdog.com>
2024-09-30 14:21:37 +0200

    Remove unused command scheduling in console routes
    Commented out unnecessary Schedule commands that were set to run every minute. This cleanup helps in reducing confusion and ensuring only relevant scheduled tasks are present in the codebase.

--- routes/console.php
+++ routes/console.php
@@ -7,6 +7,3 @@ use Illuminate\Support\Facades\Schedule;
 /*Schedule::command('weekly:notification')->weeklyOn(5, '15:30')->timezone('Europe/London');
 Schedule::command('weekly:allsummary')->weeklyOn(5, '18:00')->timezone('Europe/London');
 Schedule::command('weekly:usersummary')->weeklyOn(5, '18:00')->timezone('Europe/London');*/
-//Schedule::command('weekly:notification')->everyMinute();
-Schedule::command('weekly:allsummary')->everyMinute();
-//Schedule::command('weekly:usersummary')->everyMinute();
