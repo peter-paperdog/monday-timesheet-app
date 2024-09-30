74d24a51ae2842f1ee479a295b20c79583a7f331
nagybence <bence@paperdog.com>
2024-09-30 14:19:35 +0200

    Swap weekly notification scheduling with allsummary scheduling
    Comment out 'weekly:notification' scheduling and activate 'weekly:allsummary' scheduling to run every minute. This ensures that the allsummary command is executed regularly while disabling the frequent execution of notification.

--- routes/console.php
+++ routes/console.php
@@ -7,6 +7,6 @@ use Illuminate\Support\Facades\Schedule;
 /*Schedule::command('weekly:notification')->weeklyOn(5, '15:30')->timezone('Europe/London');
 Schedule::command('weekly:allsummary')->weeklyOn(5, '18:00')->timezone('Europe/London');
 Schedule::command('weekly:usersummary')->weeklyOn(5, '18:00')->timezone('Europe/London');*/
-Schedule::command('weekly:notification')->everyMinute();
-//Schedule::command('weekly:allsummary')->everyMinute();
+//Schedule::command('weekly:notification')->everyMinute();
+Schedule::command('weekly:allsummary')->everyMinute();
 //Schedule::command('weekly:usersummary')->everyMinute();
