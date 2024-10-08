9629c5e1c4f2f05a231831f8699050d3718faea5
nagybence <bence@paperdog.com>
2024-10-08 12:40:35 +0200

    Update API route to send test email
    Replaced the '/test' route with '/send-test-email' to send a test email to 'bence@paperdog.com' using the `allUserSummaryEmail` class. Unused code for date manipulation has been removed.

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,18 @@
 <?php
+
+use App\Mail\allUserSummaryEmail;
+use App\Mail\attentionEmail;
+use App\Services\MondayService;
+use App\Services\UserService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/test', function (Request $request) {
-    $startOfWeek = new DateTime();
-    $startOfWeek->modify('Monday last week');
-
-    $endOfWeek = clone $startOfWeek;
-    $endOfWeek->modify('Sunday last week');
-
-    var_dump($startOfWeek);
-    var_dump($endOfWeek);
-    die();
+Route::get('/send-test-email', function () {
+    Mail::to('bence@paperdog.com')->send(new allUserSummaryEmail());
 });
+
