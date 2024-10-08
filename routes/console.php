38483a0ef66819da12f1a094e3df28ad91c34f3d
nagybence <bence@paperdog.com>
2024-10-08 12:54:12 +0200

    Update weekly schedule commands for notifications and summaries
    Adjusted the timing and days for various scheduled commands. Added detailed comments for better clarity on the purpose of each schedule.

--- routes/console.php
+++ routes/console.php
@@ -4,6 +4,12 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
+//all user notify
 Schedule::command('weekly:notification')->weeklyOn(5, '15:30')->timezone('Europe/London');
-Schedule::command('weekly:allsummary')->weeklyOn(5, '18:00')->timezone('Europe/London');
-Schedule::command('weekly:usersummary')->weeklyOn(5, '18:10')->timezone('Europe/London');
+
+//Wenna mail
+Schedule::command('weekly:allsummary')->weeklyOn(1, '18:00')->timezone('Europe/London');
+
+//all user summary mails
+Schedule::command('weekly:usersummary')->weeklyOn(1, '09:00')->timezone('Europe/London');
+Schedule::command('weekly:usersummary')->weeklyOn(1, '15:00')->timezone('Europe/London');
