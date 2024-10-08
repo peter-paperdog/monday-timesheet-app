b841b7563b814ad8544afbc2e1f45248e9601c26
nagybence <bence@paperdog.com>
2024-10-08 12:32:32 +0200

    Add /test route for date range debugging
    Introduced a new /test endpoint that calculates the start and end of the previous week. This route dumps the calculated dates and terminates the script for debugging purposes.

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/test', function (Request $request) {
+    $startOfWeek = new DateTime();
+    $startOfWeek->modify('Monday last week');
+
+    $endOfWeek = clone $startOfWeek;
+    $endOfWeek->modify('Sunday last week');
+
+    var_dump($startOfWeek);
+    var_dump($endOfWeek);
+    die();
+});
