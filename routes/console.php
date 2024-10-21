4c01c68e15a07dae42b700823c5a648a7f4ecf22
nagybence <bence@paperdog.com>
2024-10-21 16:47:45 +0200

    Adjust weekly summary mail time
    Changed the time for the weekly summary mail from 18:00 to 17:57 to ensure it aligns better with the operational schedule. This minor adjustment helps synchronize email dispatch with other scheduled tasks.

--- routes/console.php
+++ routes/console.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command('weekly:notification')->weeklyOn(5, '15:30')->timezone('Europe/London');
 
 //Wenna mail
-Schedule::command('weekly:allsummary')->weeklyOn(1, '18:00')->timezone('Europe/London');
+Schedule::command('weekly:allsummary')->weeklyOn(1, '17:57')->timezone('Europe/London');
 
 //all user summary mails
 Schedule::command('weekly:usersummary')->weeklyOn(1, '09:00')->timezone('Europe/London');
