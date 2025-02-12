b7aece341a0fbb2d5b81c3c1d07af03498cf373e
Péter Vókó <peter@paperdog.com>
2025-02-12 21:12:38 +0100

    Add migration for monday_boards table
    This migration creates the monday_boards table with columns for id, name, type, and timestamps. The id column serves as the primary key, and a rollback method is included to drop the table if needed. This sets up the database structure for managing Monday.com boards.

---
+++ database/migrations/2025_02_12_200239_create_monday_boards_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('monday_boards', function (Blueprint $table) {
+            $table->string('id')->primary(); // Set monday_id as the primary key
+            $table->string('name');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('monday_boards');
+    }
+};
