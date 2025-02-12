59d4667354ffd714441ea866456ecfb555110b20
Péter Vókó <peter@paperdog.com>
2025-02-12 22:39:36 +0100

    Add activity log synchronization to Monday boards
    Extend the `SyncMondayBoards` command to fetch and process activity logs from the past day. Update boards with the latest activity timestamps and log the results for better tracking of board activity.

--- app/Console/Commands/SyncMondayBoards.php
+++ app/Console/Commands/SyncMondayBoards.php
@@ -4,7 +4,9 @@ namespace App\Console\Commands;
 
 use App\Models\MondayBoard;
 use App\Services\MondayService;
+use DateTime;
 use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
 
 class SyncMondayBoards extends Command
 {
@@ -48,6 +50,41 @@ class SyncMondayBoards extends Command
 
             $this->info("Board '{$board->name}' synced with Monday ID: {$board->id}");
         }
+
+        //check activity
+        $fromDate = Carbon::now()->subDays(1)->toIso8601String(); // Last 1 day
+        $toDate = Carbon::now()->toIso8601String(); // Current time
+
+        $items = $this->mondayService->getLastActivities($fromDate, $toDate);
+
+        $filteredItems = array_filter($items, function ($item) {
+            return !empty($item['activity_logs']);
+        });
+
+        $results = array_map(function ($item) {
+            $highestCreatedAt = max(array_column($item['activity_logs'], 'created_at'));
+
+            return [
+                'id' => $item['id'],
+                'name' => $item['name'],
+                'activity_at' => gmdate("Y-m-d\TH:i:s\Z", $highestCreatedAt / 10000000)
+            ];
+        }, $filteredItems);
+
+        $results = array_values($results);
+
+        foreach ($results as $boardData) {
+            $board = MondayBoard::where('id', $boardData['id'])->first();
+
+            // Ensure the board exists before updating
+            if ($board && !empty($boardData['activity_logs'])) {
+                $board->update(['activity_at' => Carbon::createFromTimestamp($boardData['activity_at'])]);
+                $this->info("Board '{$board->name}' updated with activity_at: {$board->activity_at}");
+            }
+
+            $this->info("Board '{$board->name}' activity: {$board->activity_at}");
+        }
+
 
         $this->info('Board synchronization complete.');
     }
