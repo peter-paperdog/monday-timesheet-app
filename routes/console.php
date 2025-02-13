9f07f3a42f771fb6e4c06d6c0345ba318ceb5abf
Péter Vókó <peter@paperdog.com>
2025-02-13 14:23:14 +0100

    Simplify scheduled commands in console routes
    Removed redundant scheduling for commands in `console.php`. Retained only the `sync:monday-boards` command to streamline task execution and avoid duplication.

--- routes/console.php
+++ routes/console.php
@@ -4,26 +4,8 @@ use Illuminate\Support\Facades\Schedule;
 
 app()->singleton(Schedule::class, function (Schedule $schedule) {
 
-// Run every hour between 6 AM and 12 PM (excluding weekends)
-    $schedule->command('sync:monday-users')
-        ->hourly()
-        ->between('6:00', '12:00')
-        ->weekdays()
-        ->timezone('Europe/London');
-
+    // Run every hour between 6 AM and 12 PM (excluding weekends)
     $schedule->command('sync:monday-boards')
-        ->hourly()
-        ->between('6:00', '12:00')
-        ->weekdays()
-        ->timezone('Europe/London');
-
-    $schedule->command('sync:active-board-time-tracking')
-        ->hourly()
-        ->between('6:00', '12:00')
-        ->weekdays()
-        ->timezone('Europe/London');
-
-    $schedule->command('sync:items')
         ->hourly()
         ->between('6:00', '12:00')
         ->weekdays()
