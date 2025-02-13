827ce3456e4ffff2b41cceb50481c1cbfa2725a0
Péter Vókó <peter@paperdog.com>
2025-02-13 18:39:15 +0100

    Add SyncController to handle Monday assignments syncing
    Introduced SyncController with a method to trigger the `sync:monday-assignments` Artisan command and refresh the page. This allows for streamlined execution of Monday assignments synchronization directly via the controller.

---
+++ app/Http/Controllers/SyncController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+
+class SyncController extends Controller
+{
+
+    public function syncMondayAssignments(Request $request)
+    {
+        // Run sync commands
+        Artisan::call('sync:monday-assignments');
+
+        // Reload the page
+        return redirect()->back();
+    }
+}
