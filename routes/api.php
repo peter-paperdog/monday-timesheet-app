9083042e5b4bb47e01f8963a5a55dd9d8b90da8c
Péter Vókó <peter@paperdog.com>
2025-02-13 13:20:48 +0100

    Simplify API routes file by removing unused imports
    Removed redundant imports related to mailing services and user-related utilities. This cleanup improves code maintainability and reduces unnecessary dependencies in the API routes file.

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,6 @@
 <?php
 
-use App\Mail\allUserSummaryEmail;
-use App\Mail\attentionEmail;
-use App\Services\MondayService;
-use App\Services\UserService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
