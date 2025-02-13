d136f77584618af3d8d7ad0738ae4f9c549a9cf3
Péter Vókó <peter@paperdog.com>
2025-02-13 09:55:25 +0100

    Update navigation links and layout spacing
    Replaces 'sheets.search' links with 'dashboard' and adds a link to 'timesheets' in both desktop and responsive navigation. Adjusts header height from 16 to 20 for better spacing. Improves overall navigation clarity and accessibility.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,18 +1,21 @@
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
+        <div class="flex justify-between h-20">
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('sheets.search') }}">
+                    <a href="{{ route('dashboard') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('sheets.search')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        Dashboard
+                    </x-nav-link>
+                    <x-nav-link :href="route('timesheets')" :active="request()->routeIs('timesheets')">
                        Weekly timesheets
                     </x-nav-link>
                 </div>
@@ -63,8 +66,11 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('sheets.search')" :active="request()->routeIs('sheets.search')">
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('timesheets')" :active="request()->routeIs('timesheets')">
+                {{ __('Timesheets') }}
             </x-responsive-nav-link>
         </div>
 
@@ -76,8 +82,8 @@
             </div>
 
             <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
+                <x-responsive-nav-link :href="route('dashboard')">
+                    {{ __('Dashboard') }}
                 </x-responsive-nav-link>
 
                 <!-- Authentication -->
