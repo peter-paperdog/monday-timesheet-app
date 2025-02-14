db676ef33f2bca4c92c82117a95fcabf9d358690
Péter Vókó <peter@paperdog.com>
2025-02-14 13:18:04 +0100

    Add email template for time record notifications
    This commit introduces a new Blade template for sending time logging reminders. The template ensures clear communication with users and encourages timely updates in Monday.com.

---
+++ resources/views/emails/time-record-notification.blade.php
@@ -0,0 +1,14 @@
+<x-mail::message>
+    # Please Log Your Time for This Week
+
+    Hello {{ explode(' ', $user->name)[0] }},
+
+    This is a friendly reminder to log your time for this week in Monday.com.
+
+    If you've already logged your time, thank you!
+
+    Have a great weekend!
+
+    Thanks,
+    the PD Monday team
+</x-mail::message>
