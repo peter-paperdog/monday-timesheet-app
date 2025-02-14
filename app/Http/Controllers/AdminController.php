2af3fa1da6a5ce9f3ebd03c5db8cc647563e3353
Péter Vókó <peter@paperdog.com>
2025-02-14 14:58:17 +0100

    Add AdminController with index method
    Introduces a new AdminController responsible for handling admin-related functionalities. Currently, it includes an `index` method that returns the `admin.index` view. This sets the foundation for future admin-specific features.

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+class AdminController extends Controller
+{
+
+    public function index()
+    {
+
+
+        return view('admin.index', [
+        ]);
+    }
+}
