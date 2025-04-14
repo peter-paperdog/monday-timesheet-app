d869be3520482bddf823e9f6b27cc4b914c95236
Péter Vókó <peter@paperdog.com>
2025-04-14 20:44:35 +0300

    Update invoicing view to display dynamic client and project info
    Revised the invoicing creation view to dynamically show client and project names. Added support for displaying associated boards with their names for better context and clarity. Improved UI structure for a more readable layout.

--- resources/views/invoicing/create.blade.php
+++ resources/views/invoicing/create.blade.php
@@ -1,16 +1,24 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            <a href="{{ url('/invoicing') }}">Invoicing</a> / Client
+            <a href="{{ url('/invoicing') }}">Invoicing</a> / {{ $clientName }} @if($projectName)
+                / {{ $projectName }}
+            @endif
         </h2>
     </x-slot>
     <div class="py-5">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100 pt-3 my-3">
-                    <p><strong>Client ID:</strong> {{ request('client') }}</p>
-                    <p><strong>Project ID:</strong> {{ request('project') }}</p>
-                    <p><strong>Folder IDs:</strong> {{ request('folders') }}</p>
+                    <h1 class="text-xl">Client: {{ $clientName }}</h1>
+                    @if($projectName)
+                        <h1 class="text-xl">Project: {{ $projectName }}</h1>
+                    @endif
+                </div>
+                <div class="p-6 text-gray-900 dark:text-gray-100 pt-3 my-3">
+                    @foreach ($boards as $board)
+                        <h1>{{ $board['name'] }}</h1>
+                    @endforeach
                 </div>
             </div>
         </div>
