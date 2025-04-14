b0a698b4c7cb8433a34dc27c0817a475f8307fa3
Péter Vókó <peter@paperdog.com>
2025-04-14 20:44:17 +0300

    Refactor `create` method in InvoicingController.
    Update the `create` method to handle request inputs and fetch client, project, and boards data using the MondayService. Pass the retrieved data to the view for improved functionality and clarity.

--- app/Http/Controllers/InvoicingController.php
+++ app/Http/Controllers/InvoicingController.php
@@ -7,14 +7,34 @@ use Illuminate\Http\Request;
 
 class InvoicingController extends Controller
 {
-    public function __construct(private MondayService $mondayService){
-
-    }
-
-    public function create()
+    public function __construct(private MondayService $mondayService)
     {
-        return view('invoicing.create');
+
     }
+
+    public function create(Request $request)
+    {
+        $clientId = $request->input('client');
+        $projectId = $request->input('project');
+        $boardIds = explode(',', $request->input('folders', ''));
+
+        $clientName = $this->mondayService->getFoldername($clientId);
+
+        if (!empty($projectId) && $projectId !== '-1') {
+            $projectName = $this->mondayService->getFoldername($projectId);
+        } else {
+            $projectName = null;
+        }
+
+        $boards = [];
+
+        foreach($boardIds as $boardId) {
+            $boards[] = $this->mondayService->getBoard($boardId);
+        }
+
+        return view('invoicing.create', compact('clientName', 'projectName', 'boards'));
+    }
+
     public function index()
     {
         $data = $this->mondayService->getFolders();
