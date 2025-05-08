b0892b1696294b78ba56aa36d0e61bf96867f082
Péter Vókó <peter@paperdog.com>
2025-05-08 18:25:54 +0300

    Parse XML invoice data and store it in the database.
    Replaced manual invoice ID generation with XML data extraction to create or update invoice records in the database. Added logic to parse key invoice details such as totals, buyer, and seller information using XPath. Simplified the tracking number generation to use the stored invoice ID.

--- app/Http/Controllers/SzamlazzController.php
+++ app/Http/Controllers/SzamlazzController.php
@@ -13,8 +13,45 @@ class SzamlazzController extends Controller
     {
         Log::info('Számla BE:', $this->logData($request));
 
-        $id = random_int(1000, 9999);
-        $iktatoszam = 'IKT-' . now()->format('Ymd') . '-' . $id;
+        Log::info('Számla BE:', $this->logData($request));
+
+        $xmlContent = $request->getContent();
+        $xml = simplexml_load_string($xmlContent);
+        $namespaces = $xml->getNamespaces(true);
+        $xml->registerXPathNamespace('ns', $namespaces[''] ?? 'http://www.szamlazz.hu/szamlabe');
+
+        // Kinyerjük az adatokat
+        $id = (int) ($xml->xpath('//ns:alap/ns:id')[0] ?? 0);
+        $szamlaszam = (string) ($xml->xpath('//ns:alap/ns:szamlaszam')[0] ?? '');
+        $kelt = (string) ($xml->xpath('//ns:alap/ns:kelt')[0] ?? '');
+        $telj = (string) ($xml->xpath('//ns:alap/ns:telj')[0] ?? '');
+        $fizh = (string) ($xml->xpath('//ns:alap/ns:fizh')[0] ?? '');
+        $fizmod = (string) ($xml->xpath('//ns:alap/ns:fizmod')[0] ?? '');
+        $devizanem = (string) ($xml->xpath('//ns:alap/ns:devizanem')[0] ?? '');
+        $teszt = (string) ($xml->xpath('//ns:alap/ns:teszt')[0] ?? 'false') === 'true';
+        $sztornozott = (string) ($xml->xpath('//ns:alap/ns:sztornozott')[0] ?? 'false') === 'true';
+
+        $netto = (float) ($xml->xpath('//ns:totalossz/ns:netto')[0] ?? 0);
+        $afa = (float) ($xml->xpath('//ns:totalossz/ns:afa')[0] ?? 0);
+        $brutto = (float) ($xml->xpath('//ns:totalossz/ns:brutto')[0] ?? 0);
+
+        $vevo_nev = (string) ($xml->xpath('//ns:vevo/ns:nev')[0] ?? '');
+        $vevo_adoszam = (string) ($xml->xpath('//ns:vevo/ns:adoszam')[0] ?? '');
+
+        $szallito_nev = (string) ($xml->xpath('//ns:szallito/ns:nev')[0] ?? '');
+        $szallito_adoszam = (string) ($xml->xpath('//ns:szallito/ns:adoszam')[0] ?? '');
+
+        // Mentés
+        $invoice = SzamlazzInvoice::firstOrCreate(
+            ['id' => $id],
+            compact(
+                'szamlaszam', 'kelt', 'telj', 'fizh', 'fizmod', 'devizanem',
+                'netto', 'afa', 'brutto', 'vevo_nev', 'vevo_adoszam',
+                'szallito_nev', 'szallito_adoszam', 'teszt', 'sztornozott'
+            )
+        );
+
+        $iktatoszam = 'IKT-' . $invoice->id;
 
         $xml = '<?xml version="1.0" encoding="UTF-8"?>' .
             '<szamlabevalasz xmlns="http://www.szamlazz.hu/szamlabevalasz" ' .
