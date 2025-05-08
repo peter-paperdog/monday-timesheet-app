9566877c00814326f02ed4448c543e1463c8c235
Péter Vókó <peter@paperdog.com>
2025-05-08 17:34:32 +0300

    Add 'id' to fillable attributes in SzamlazzTransaction
    Including 'id' in the fillable array ensures it can be mass-assigned, aligning with the application's requirements. This change is crucial for properly managing transaction records.

--- app/Models/SzamlazzTransaction.php
+++ app/Models/SzamlazzTransaction.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class SzamlazzTransaction extends Model
 {
     protected $fillable = [
-        'external_id', 'bankszamla', 'erteknap', 'irany', 'tipus',
+        'id', 'external_id', 'bankszamla', 'erteknap', 'irany', 'tipus',
         'technikai', 'osszeg', 'devizanem', 'partner_nev',
         'partner_bankszamla', 'kozlemeny',
     ];
