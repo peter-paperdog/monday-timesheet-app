e4219bfdd256c5a656bd54c97ca34dce5d109564
Péter Vókó <peter@paperdog.com>
2025-05-08 17:29:09 +0300

    Add 'id' to fillable properties in SzamlazzInvoice model
    Including 'id' in the fillable array allows for mass assignment of the primary key when needed. This change ensures better flexibility and consistency in handling model attributes.

--- app/Models/SzamlazzInvoice.php
+++ app/Models/SzamlazzInvoice.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class SzamlazzInvoice extends Model
 {
     protected $fillable = [
-        'szamlaszam', 'kelt', 'telj', 'fizh', 'fizmod', 'devizanem',
+        'id', 'szamlaszam', 'kelt', 'telj', 'fizh', 'fizmod', 'devizanem',
         'netto', 'afa', 'brutto', 'vevo_nev', 'vevo_adoszam',
         'szallito_nev', 'szallito_adoszam', 'teszt', 'sztornozott',
     ];
