c938b43e9b6e3f51ed68e365f6646e815427135d
Péter Vókó <peter@paperdog.com>
2025-05-08 15:13:49 +0300

    Add SzamlazzInvoice model
    Introduce the SzamlazzInvoice model to represent invoice-related data. This model includes fillable attributes for invoice details such as amounts, customer and supplier information, and statuses.

---
+++ app/Models/SzamlazzInvoice.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SzamlazzInvoice extends Model
+{
+    protected $fillable = [
+        'szamlaszam', 'kelt', 'telj', 'fizh', 'fizmod', 'devizanem',
+        'netto', 'afa', 'brutto', 'vevo_nev', 'vevo_adoszam',
+        'szallito_nev', 'szallito_adoszam', 'teszt', 'sztornozott',
+    ];
+}
