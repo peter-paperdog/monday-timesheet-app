6da3261f99d7cdd7e3ae52837c383088dfc11149
Péter Vókó <peter@paperdog.com>
2025-05-08 17:48:15 +0300

    Remove 'external_id' from fillable attributes
    The 'external_id' field was removed from the fillable property of the SzamlazzTransaction model. This change enhances security by preventing mass assignment of this field.

--- app/Models/SzamlazzTransaction.php
+++ app/Models/SzamlazzTransaction.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class SzamlazzTransaction extends Model
 {
     protected $fillable = [
-        'id', 'external_id', 'bankszamla', 'erteknap', 'irany', 'tipus',
+        'id', 'bankszamla', 'erteknap', 'irany', 'tipus',
         'technikai', 'osszeg', 'devizanem', 'partner_nev',
         'partner_bankszamla', 'kozlemeny',
     ];
