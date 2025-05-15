97e01bab4c9c088c356a3213f051106b5dbf14ab
Péter Vókó <peter@paperdog.com>
2025-05-15 16:26:30 +0300

    Add migration for creating the items table
    This migration introduces the `items` table with fields for storing item details such as `monday_id`, `description`, `type`, and pricing information. It also includes a foreign key linking to the `projects` table with cascade deletion. This lays the foundation for managing item data within the database.

---
+++ database/migrations/2025_05_15_122447_create_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('monday_id')->unique();
+            $table->string('description');
+            $table->string('type');
+            $table->float('quantity');
+            $table->string('unit');
+            $table->decimal('unit_price', 10, 2);
+            $table->string('currency');
+            $table->foreignId('project_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
