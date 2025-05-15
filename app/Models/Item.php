0cb16eb275c52fb14566e93038a9ad22e29cb5ba
Péter Vókó <peter@paperdog.com>
2025-05-15 16:27:11 +0300

    Add Item model with relationships and fillable properties
    Introduced the Item model to the application, including a `belongsTo` relationship to the Project model. The `$fillable` array was defined to enable mass assignment for key attributes. This sets the foundation for managing item-related data.

---
+++ app/Models/Item.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'monday_id', 'description', 'type', 'quantity', 'unit',
+        'unit_price', 'currency', 'project_id'
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+}
