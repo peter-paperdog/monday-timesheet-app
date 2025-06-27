18ff8e9ae2e7a6a12db2db915d312899b2f2ec4e
Péter Vókó <peter@paperdog.com>
2025-06-27 13:18:56 +0300

    Add method to fetch contacts by client
    Introduce the `indexByClient` method in `ContactController` to retrieve contacts associated with a specific client. This enhances the API's functionality for client-specific data filtering.

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ContactResource;
+use App\Models\Client;
 use App\Models\Contact;
 
 class ContactController extends Controller
@@ -11,6 +12,11 @@ class ContactController extends Controller
     public function index()
     {
         return ContactResource::collection(Contact::all());
+    }
+
+    public function indexByClient(Client $client)
+    {
+        return ContactResource::collection($client->contacts);
     }
 
     public function show(Contact $client)
