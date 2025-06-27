38c8078ec75e75fae1cd4de0c6b1f6af3f2982c9
Péter Vókó <peter@paperdog.com>
2025-06-27 16:01:06 +0300

    Add GroupResource to format group API responses
    Introduce a new GroupResource class to standardize the structure of group data in API responses. This includes related tasks, using TaskResource for consistent nested representation.

---
+++ app/Http/Resources/GroupResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class GroupResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'project_id' => $this->project_id,
+            'tasks' => TaskResource::collection($this->whenLoaded('tasks')),
+        ];
+    }
+}
