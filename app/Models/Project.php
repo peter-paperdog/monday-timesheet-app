6fc84ee486253a97e1aa15bc2436bbb420c8fb42
Péter Vókó <peter@paperdog.com>
2025-06-30 15:32:27 +0300

    Add new attributes to Project model's fillable array
    Updated the Project model to include `time_board_id` and `expenses_board_id` in the fillable array. Removed unused `tasks` and `groups` relationships to streamline the model.

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,20 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    protected $fillable = ['id', 'name', 'client_id'];
-
-    public function groups()
-    {
-        return $this->hasMany(Group::class);
-    }
+    protected $fillable = ['id', 'name', 'client_id', 'time_board_id', 'expenses_board_id'];
 
     public function client()
     {
         return $this->belongsTo(Client::class);
-    }
-
-    public function tasks()
-    {
-        return $this->hasMany(Task::class);
     }
 }
