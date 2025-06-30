e05b60a39d87e8a4b0975a04b199bb851a7747a4
Péter Vókó <peter@paperdog.com>
2025-06-30 14:11:23 +0300

    Add endpoint to fetch tasks by project
    This commit introduces a new route to retrieve tasks associated with a specific project. Unused imports (Project and MondayService) were also removed for code cleanup.

--- routes/api.php
+++ routes/api.php
@@ -10,8 +10,6 @@ use App\Http\Controllers\GoogleAuthController;
 use App\Http\Controllers\InvoicingController;
 use App\Http\Controllers\OfficeController;
 use App\Http\Controllers\WebhookController;
-use App\Models\Project;
-use App\Services\MondayService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -34,6 +32,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/projects', [ProjectController::class, 'index']);
     Route::get('/projects/{id}', [ProjectController::class, 'show']);
     Route::get('/projects/{project}/groups', [GroupController::class, 'index']);
+    Route::get('/projects/{project}/tasks', [TaskController::class, 'indexByProject']);
 
     Route::get('/contacts', [ContactController::class, 'index']);
 
