32b911701d1aa80a84932ca749958f92d6ae956f
Péter Vókó <peter@paperdog.com>
2025-06-30 14:10:53 +0300

    Add indexByProject method to TaskController
    Introduce a new method to fetch tasks associated with a specific project. This enhances functionality by allowing tasks to be retrieved with their related groups based on the project context.

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\TaskCollection;
 use App\Http\Resources\TaskResource;
+use App\Models\Project;
 use App\Models\Task;
 use Illuminate\Http\Request;
 
@@ -18,5 +19,10 @@ class TaskController extends Controller
     public function show(Task $task)
     {
         return new TaskResource($task);
+    }
+
+    public function indexByProject(Project $project)
+    {
+        return TaskResource::collection($project->tasks()->with('group')->get());
     }
 }
