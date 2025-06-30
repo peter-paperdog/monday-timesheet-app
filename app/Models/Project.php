470f204811d1971a10f55a3f2f33dd845e5e540d
Péter Vókó <peter@paperdog.com>
2025-06-30 14:11:06 +0300

    Add tasks relationship to Project model
    Introduced a `hasMany` relationship to link Projects with Tasks. This improves database modeling, enabling easier access to related tasks for a given project.

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -17,4 +17,9 @@ class Project extends Model
     {
         return $this->belongsTo(Client::class);
     }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
