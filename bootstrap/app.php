3b5846320f9aaa615bed3d9707f4c385e72272b2
Péter Vókó <peter@paperdog.com>
2025-06-30 19:09:50 +0300

    Update sync:monday-tasks schedule to run more frequently
    Changed the sync:monday-tasks job to execute every 15 minutes instead of daily at 12:05 AM. This ensures more frequent updates, with the job running between 8:00 AM and 11:00 PM.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,7 +26,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withSchedule(function (Schedule $schedule) {
         // Schedule tasks to run every hour between 6 AM and 12 PM (weekdays only)
         $schedule->command('sync:monday-users')->daily()->at("00:20");
-        $schedule->command('sync:monday-tasks')->daily()->at("00:05");
+        $schedule->command('sync:monday-tasks')->everyFifteenMinutes()->between('08:00', '23:00');
         $schedule->command('sync:monday-boards')->everyFifteenMinutes()->between('08:00', '23:00');
         $schedule->command('sync:monday-folders')->hourly()->between('08:05', '23:05');
         $schedule->command('sync:monday-groups')->hourly()->between('08:10', '23:10');
