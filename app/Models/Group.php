c473077e87e2cf5c47b865c35c5484d78816b890
Péter Vókó <peter@paperdog.com>
2025-07-01 17:47:28 +0300

    Set Group model primary key to non-incrementing string.
    Changed the Group model to use a non-incrementing string as its primary key by setting `$incrementing` to false and `$keyType` to 'string'. This ensures compatibility with UUIDs or custom primary key formats.

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
+    public $incrementing = false;
+    protected $keyType = 'string';
     protected $fillable = ['id', 'name', 'project_id'];
 
     public function project()
