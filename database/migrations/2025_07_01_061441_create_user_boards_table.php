918fc0ffdcc11946df5960d1af68c960eab8ae3a
Péter Vókó <peter@paperdog.com>
2025-07-01 09:43:26 +0300

    Add migration for creating user_boards table
    Introduces a new migration to create the `user_boards` table with fields for `id`, `user_id`, and `name`. Includes a foreign key constraint on `user_id` referencing the `users` table with `nullOnDelete` behavior.

---
+++ database/migrations/2025_07_01_061441_create_user_boards_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_boards', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->primary();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('name')->nullable();
+
+            $table->foreign('user_id')->references('id')->on('users')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_boards');
+    }
+};
