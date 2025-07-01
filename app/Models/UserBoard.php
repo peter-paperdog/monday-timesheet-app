dbbd8b3a1435426e2bc7007f47b54033cfcc632c
Péter Vókó <peter@paperdog.com>
2025-07-01 09:42:57 +0300

    Add UserBoard model to represent user-specific boards
    This new model includes relationships and attribute casting. It defines a `BelongsTo` relationship with the `User` model and specifies fillable attributes and type casts for improved data handling.

---
+++ app/Models/UserBoard.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserBoard extends Model
+{
+    public $timestamps = false;
+    protected $fillable = [
+        'id',
+        'name',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'id' => 'integer',
+        'user_id' => 'integer',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
