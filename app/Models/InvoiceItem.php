d537f370dada44458d45ea92e78e696578dc52ad
Péter Vókó <peter@paperdog.com>
2025-07-01 15:53:18 +0300

    Add InvoiceItem model with relationships
    Introduced the `InvoiceItem` model to represent invoice line items. It includes fillable attributes and defines relationships to Invoice, Project, and Task models to ensure proper data associations.

---
+++ app/Models/InvoiceItem.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InvoiceItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'invoice_id',
+        'description',
+        'qty',
+        'price',
+        'project_id',
+        'task_id',
+        'TAX',
+        'discount',
+    ];
+
+    public function invoice()
+    {
+        return $this->belongsTo(Invoice::class);
+    }
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function task()
+    {
+        return $this->belongsTo(Task::class);
+    }
+}
