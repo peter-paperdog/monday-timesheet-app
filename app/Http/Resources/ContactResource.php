f5a4f6e5abbcf6091716bd98a277a1c3f3a5c308
Péter Vókó <peter@paperdog.com>
2025-07-01 17:51:27 +0300

    Ensure 'id' is consistently cast to an integer in ContactResource.
    Casting 'id' to an integer ensures data consistency and avoids potential type issues when consuming the API. This change enhances reliability when handling 'id' across different systems or platforms.

--- app/Http/Resources/ContactResource.php
+++ app/Http/Resources/ContactResource.php
@@ -15,7 +15,7 @@ class ContactResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'         => $this->id,
+            'id'         => intval($this->id),
             'name'       => $this->name,
             'type'       => $this->type,
             'company'    => $this->company,
