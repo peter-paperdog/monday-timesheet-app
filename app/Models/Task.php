35d8971ff45d2a0a4a936747d305b156ab2c67a0
Péter Vókó <peter@paperdog.com>
2025-07-01 10:15:15 +0300

    Refactor Task model to update relationships.
    Removed the project association and added a polymorphic taskable relationship. Adjusted the fillable attributes to reflect these changes, ensuring the model aligns with new use cases.

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,20 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    protected $fillable = ['id', 'name', 'group_id', 'project_id'];
+    protected $fillable = ['id', 'name', 'group_id'];
 
     public function group()
     {
         return $this->belongsTo(Group::class);
     }
 
-    public function projects()
-    {
-        return $this->belongsTo(Project::class);
-    }
-
     public function timeTrackings()
     {
         return $this->morphMany(MondayTimeTracking::class, 'trackable');
+    }
+    public function taskable()
+    {
+        return $this->morphTo();
     }
 }
