e7a84de7660fceb62274129334c1bd91e5cd7e3a
Péter Vókó <peter@paperdog.com>
2025-07-02 11:10:14 +0300

    Add method to retrieve model relations by depth
    Introduced `getRelationsByDepth` to dynamically gather relations based on the specified depth. This enhances flexibility in loading related models by using a depth-based approach defined in `relationDepthMap`.

--- app/Support/LoadsWithDepth.php
+++ app/Support/LoadsWithDepth.php
@@ -17,4 +17,17 @@ trait LoadsWithDepth
 
         return $model->load($with);
     }
+
+    protected function getRelationsByDepth(int $depth): array
+    {
+        $with = [];
+
+        foreach ($this->relationDepthMap ?? [] as $minDepth => $relations) {
+            if ($depth >= $minDepth) {
+                $with = array_merge($with, (array) $relations);
+            }
+        }
+
+        return $with;
+    }
 }
