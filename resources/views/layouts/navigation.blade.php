4c6c7411494e0e5284e68213fd38fd1c52fc8953
nagybence <bence@paperdog.com>
2025-07-08 17:00:34 +0200

    Remove redundant check for completed tasks in item processing
    The code no longer needs to skip completed tasks as the logic for handling them has been simplified. This change reduces unnecessary checks, improving readability and efficiency.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,9 +12,6 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        Dashboard
-                    </x-nav-link>
                     <x-nav-link :href="route('timesheets')" :active="request()->routeIs('timesheets')">
                         Weekly timesheets
                     </x-nav-link>
