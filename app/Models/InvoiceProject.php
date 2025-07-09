a66dcf03fba3f4f3fdb2c9d58c593b4c93b15287
Péter Vókó <peter@paperdog.com>
2025-07-09 19:13:02 +0300

    Update InvoiceProject model relationships and fillable fields
    Replaced `invoice_group_id` with `invoice_id` in fillable fields for better alignment with database structure. Modified relationships: removed `belongsTo` for `InvoiceGroup` and added `hasMany` for `InvoiceGroups` to reflect the updated schema.

--- app/Models/InvoiceProject.php
+++ app/Models/InvoiceProject.php
@@ -7,15 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class InvoiceProject extends Model
 {
     public $timestamps = false;
-    protected $fillable = ['invoice_group_id', 'project_id'];
-
-    public function group()
-    {
-        return $this->belongsTo(InvoiceGroup::class, 'invoice_group_id');
-    }
+    protected $fillable = ['invoice_id', 'project_id'];
 
     public function project()
     {
         return $this->belongsTo(Project::class);
+    }
+    public function invoiceGroups()
+    {
+        return $this->hasMany(InvoiceGroup::class);
     }
 }
